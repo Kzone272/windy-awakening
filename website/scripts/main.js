530e2963158798984ee3eeeff8957a5c95ea68fc
Kevin Haslett <kah_272@hotmail.com>
2015-11-17 21:10:16 -0500

    Added a bit more of the structre.

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -1,1 +1,12 @@
-alert('Hello Mister World!');
+var canvas = document.getElementById('window');
+
+var gl = canvas.getContext('webgl');
+
+gl.clearColor(0.5, 0.5, 0.5, 1);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+var vertexShaderString = document.getElementById('vertexShader');
+console.log(vertexShaderString);
+var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(vertexShader, vertexShaderString);
+gl.compileShader(vertexShader);
