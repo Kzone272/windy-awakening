ac765c204532eed7ea16142d7fb0ab1c70f4ee8d
Kevin Haslett <kah_272@hotmail.com>
2015-11-26 18:27:03 -0500

    Images are loading, jsut need to make textures.

--- scripts/objParser.js
+++ scripts/objParser.js
@@ -1,3 +1,12 @@
+function parseObjMtl(name, done) {
+
+  var obj = parseObj(name, function () {
+    parseMtl(name, done, obj);
+  });
+
+  return obj;
+}
+
 function parseObj(name, done) {
   var objRequest = new XMLHttpRequest();
 
@@ -12,12 +21,12 @@ function parseObj(name, done) {
       var faces = [];
       var normals = [];
       var texCoords = [];
-      var currentGroup = '';
+      var currentGroup = {};
 
       for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
+        var line = lines[i].trim();
         var tokens = line.split(/\s+/);
-        
+
         if (tokens[0] == 'v') {
           for (var j = 1; j <= 3; j++) {
             verts.push(parseFloat(tokens[j]));
@@ -41,7 +50,11 @@ function parseObj(name, done) {
             });
           }
         } else if (tokens[0] == 'g') {
-          currentGroup = tokens[1];
+          currentGroup = {
+            name: tokens[1]
+          }
+        } else if (tokens[0] == 'usemtl') {
+          currentGroup.material = tokens[1];
         } else {
           continue;
         }
@@ -52,11 +65,12 @@ function parseObj(name, done) {
       obj.texCoords = [];
       obj.groups = [];
 
-      var previousGroup = '';
+      var previousGroupName = '';
       for (var i = 0; i < faces.length; i++) {
         var face = faces[i];
 
-        if (face.g !== previousGroup) {
+        console.log(face.g.name + '   ' + previousGroupName);
+        if (face.g.name !== previousGroupName) {
           if (obj.groups.length) {
             var lastGroup = obj.groups[obj.groups.length - 1];
             lastGroup.size = obj.verts.length / 3 - lastGroup.offset;
@@ -65,9 +79,11 @@ function parseObj(name, done) {
           obj.groups.push({
             offset: obj.verts.length / 3,
             size: 0,
+            name: face.g.name,
+            material: face.g.material,
           });
         }
-        previousGroup = face.g;
+        previousGroupName = face.g.name;
 
         obj.verts.push(
           verts[(face.v - 1) * 3],
@@ -92,7 +108,7 @@ function parseObj(name, done) {
         lastGroup.size = obj.verts.length / 3 - lastGroup.offset;
       }
 
-      done();
+      parseMtl(name, done, obj);
     }
   };
 
@@ -100,4 +116,63 @@ function parseObj(name, done) {
   objRequest.send();
 
   return obj;
+}
+
+function parseMtl(name, done, obj) {
+  var mtlRequest = new XMLHttpRequest();
+
+  obj.textures = {};
+
+  mtlRequest.onreadystatechange = function () {
+    if (mtlRequest.readyState == 4) {
+      var contents = mtlRequest.responseText;
+      var lines = contents.split(/\n+/);
+
+      var currentMtl = '';
+      var images = [];
+
+      for (var i = 0; i < lines.length; i++) {
+        var line = lines[i].trim();
+        var tokens = line.split(/\s+/);
+
+        if (tokens[0] == 'newmtl') {
+          currentMtl = tokens[1];
+        } else if (tokens[0] == 'map_Kd') {
+          var fileName = tokens[1];
+          var dir = name.substr(0, name.lastIndexOf('/'));
+
+          var image = new Image();
+          image.source = dir + '/' + fileName;
+          image.mtl = currentMtl;
+          images.push(image);
+        } else {
+          continue;
+        }
+      }
+
+      for (var i = 0; i < images.length; i++) {
+        images[i].onload = function() {
+          this.loaded = true;
+
+          obj.textures[this.mtl] = this;
+
+          var allLoaded = true;
+          for (var j = 0; j < images.length; j++) {
+            allLoaded = allLoaded && !!images[j].loaded;
+          }
+
+          if (allLoaded) {
+            done();
+          }
+        }
+      }
+
+      for (var i = 0; i < images.length; i++) {
+        images[i].src = images[i].source;
+      }
+    }
+  };
+
+  mtlRequest.open('GET', name + '.mtl', true);
+  mtlRequest.send();
 }
